9e63e25f5c6db36ef5d149ba8075019f49dc0110
Clayton Smith <argilo@gmail.com>
2021-03-10 10:09:26 -0500

    Change manufacturer and product strings

--- jiggle/usb_descriptors.c
+++ jiggle/usb_descriptors.c
@@ -115,8 +115,8 @@ uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
 // array of pointer to string descriptors
 char const *string_desc_arr[] = {
     (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-    "TinyUSB",                  // 1: Manufacturer
-    "TinyUSB Device",           // 2: Product
+    "Raspberry Pi",             // 1: Manufacturer
+    "Mouse",                    // 2: Product
     "123456",                   // 3: Serials, should use chip ID
 };
 
